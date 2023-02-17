16ea46d377887fb783d5308fde14ff1a608a0700
Zineb Atlassi <atlassizineb@gmail.com>
2023-02-17 16:54:51 +0100

    Punto 1 e 2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# php-badwords
+# Descrizione:
+
+1. Creare una variabile con un paragrafo di testo a vostra scelta.
+2. Stampare a schermo il paragrafo e la sua lunghezza.
+3. Una parola da censurare viene passata dall'utente tramite parametro GET.
+4. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- SEZIONE PHP -->
+<?php
+
+$paragrofoPrima = 'Yeah, è un altro giorno, mi alzo e corro, karma storto <br>
+Punto tutto su me stesso quando gioco al Fantamorto <br>
+Sono un film sulla felicità con una trama dark <br>
+Come se stai all\'Acquapark e senti urlare, "Allah u Akbar" <br>
+Stringo i denti tra macelli e cazzi vari <br>
+Tra fratelli marginali con cervelli immaginari <br>
+Per me sali sul palco e lì riveli quanto vali <br>
+Sennò sembri il primo maschio con problemi vaginali, adieu <br>
+Sta vita me la fotto in fretta <br>
+Quindi tengo sempre il Settebello sotto il sotto sella <br>
+Tu ancora ti stupisci che nessuno ti comprenda <br>
+Se fai dischi mentre ti pulisci il culo con la merda <br>
+Sono leggenda come B. B. King <br>
+Mi riprendono in canotta mentre mi faccio una botta <br>
+Sul CD di Skin, in onda sulla BBC <br>
+Ops, l\'ho fatto un\'altra volta come Britney Spears'; 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+<p>
+    <?php echo $paragrofoPrima; ?>
+</p>
+    
+</body>
+</html>
